408d3a8b087c7b252af1f34ce92451a23bff6420
leskhan <leskhan.iskender@gmail.com>
2024-05-26 14:07:22 +0500

    add tailwindcss and primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
 import router from './router';
+import './assets/reset.css';
+import PrimeVue from 'primevue/config';
 
 createApp(App)
   .use(router)
+  .use(PrimeVue)
   .mount('#app');
